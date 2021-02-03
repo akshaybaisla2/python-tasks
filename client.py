b60ed0c349bd2aeb4dd1bc489e4b37c680c58290
Akshay Kumar <65960296+akshaybaisla2@users.noreply.github.com>
2021-02-03 18:00:08 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,11 @@
+import socket
+
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 1234
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((ip,port))
+
+    string = input("enter string: ")
+    server.send(bytes(str, "utf-8"))
